b5d5d8f242bba42f77e971cdda84045f448c0584
manuelcastro95 <jmcastro2986@misena.edu.co>
2023-11-28 10:32:52 -0500

    plantilla e-commerce

--- routes/web/ecommerceRoute.php
+++ routes/web/ecommerceRoute.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => 'e-commerce/'], function () {
+
+    Route::get('dashboard', function () {
+        return view('layouts.admin.app');
+    })->name('dashboard');
+
     Route::get('', function () {
         return view('ecommerce.index');
     })->name('inicio');
